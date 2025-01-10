e6e55924eb72741ba9f3ba228509fd7a053e3c79
oiji <oesp912@gmail.com>
2025-01-10 22:29:41 +0900

    [MOD] fastapi cli로 실행방식 변경, 리로드 설정

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,8 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def hello():
-    return "hi hi hi"
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    return "hello world"
